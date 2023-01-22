109b2f2529dd09d23c2c77da676e0cd42ea11fa9
Pasi P <pjpakkala@gmail.com>
2023-01-22 14:41:32 +0000

    Code is finalized. Openai is able to provide a functional summary and references are added at the end.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 In this repl I train my web scraping skills and combine the results with openai.
+
+User can input any wikipedia link.
+BeautifulSoup then picks <p> from there to find the text content, which is given to openai to summarize. 
+Overall summary works well. Amount of tokens can be adjusted if results need to be longer.
--- main.py
+++ main.py
@@ -7,22 +7,36 @@ openai.api_key = os.environ['openai_api_key']
 #Model.list() returns information about all models available
 openai.Model.list()
 
-url = "https://en.wikipedia.org/wiki/Independence_of_Finland"
 
+#the link we want to summarize
+#url = "https://en.wikipedia.org/wiki/Alexandria_Ariana"
+url = input("What wikipedia link do you want to have summarized: ")
+print()
+
+#below soup part identifies relevant content on the webpage and saves it in "text" variable
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, "html.parser")
+
+#we know that by picking <p> from the page, we get the desired content
 article = soup.find_all("p")
 
-for text in article:
-    task = text.text
-    question = "When Finland became independent?"
-    
-    prompt = (f"In maximum three paragraphs. Answer {question} based on {task}")
-    response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=50)
-    print(response["choices"][0]["text"].strip())
+#and with below the overall text is of better quality
+text = ""
+for p in article:
+    text += p.text
 
-#choose a wikipedia page
-#use BS to extract relevant text form there
-#request openai to summarize the text in max 3 paragraphs
-#also print references at the end
+    
+#openai part. text from above is sent to openai with a request to summarize. Neccessary identifications are above in beginning
+#max_tokens can be updated, 200 seems to provide enough content
+prompt = f"Summarize following text in 3 paragraphs or less: {text}"
+response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=200)
+    
+print(response["choices"][0]["text"].strip())
+
+
+#identifying references in wikipedia and listing those at the end
+refs = soup.find_all("ol", {"class": "references"})
+for ref in refs:
+    print(ref.text.replace("^ ", ""))
+
